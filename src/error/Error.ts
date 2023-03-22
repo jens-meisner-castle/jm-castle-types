8b61e2e73f2dc4be51f0d8399071e670e00dd053
Jens Meisner <jnsmsnr@gmail.com>
2023-03-22 08:49:51 +0100

    bugfix keys for ErrorCodes

--- src/error/Error.ts
+++ src/error/Error.ts
@@ -9,63 +9,63 @@ export const TokenExpiredErrorCode = "40102";
 // name is coming from jsonwebtoken library
 export const JsonWebTokenError = "40103";
 export const UnknownClientOrBadIpCode = "40104";
-export const BadTimeForRequestTryLaterCode = "42501"
+export const BadTimeForRequestTryLaterCode = "42501";
 export const CastleConfigErrorCode = "66601";
 export const DevErrorCode = "77701";
 export const SqlDataErrorCode = "88801";
 
 export const ErrorCodes = {
-  [UnknownErrorCode]: {
+  "-1": {
     name: "UnknownError",
     description: "Used, if a new or unknwon error name was used.",
     apiStatus: 500,
   },
-  [BadRequestMissingParameterCode]: {
+  "40001": {
     name: "BadRequestMissingParameter",
     description: "One or more needed parameters are undefined.",
     apiStatus: 400,
   },
-  [BadRequestBadParameterCode]: {
+  "40002": {
     name: "BadRequestBadParameter",
     description: "One or more parameters are not correct.",
     apiStatus: 400,
   },
-  [TokenUndefinedErrorCode]: {
+  "40101": {
     name: "TokenUndefinedError",
     description: "Token not specified in request.",
     apiStatus: 401,
   },
-  [TokenExpiredErrorCode]: {
+  "40102": {
     name: "TokenExpiredError",
     description: "Token is too old.",
     apiStatus: 401,
   },
-  [JsonWebTokenError]: {
+  "40103": {
     name: "JsonWebTokenError",
     description: "Token could not be verified.",
     apiStatus: 401,
   },
-  [UnknownClientOrBadIpCode]: {
+  "40104": {
     name: "UnknownClientOrBadIpError",
     description: "Client is unknown or running on not authorized ip.",
     apiStatus: 401,
   },
-  [BadTimeForRequestTryLaterCode]: {
+  "42501": {
     name: "BadTimeForRequestTryLaterError",
     description: "Service cannot be used now. Try later.",
     apiStatus: 425,
   },
-  [CastleConfigErrorCode]: {
+  "66601": {
     name: "CastleConfigError",
     description: "Bad configuration of the castle system.",
     apiStatus: 500,
   },
-  [DevErrorCode]: {
+  "77701": {
     name: "DevError",
     description: "This should not happen. Needs a software update.",
     apiStatus: 500,
   },
-  [SqlDataErrorCode]: {
+  "88801": {
     name: "SqlDataError",
     description:
       "Used for bad data. Original error comes from DB Server. Check error no in error message.",
